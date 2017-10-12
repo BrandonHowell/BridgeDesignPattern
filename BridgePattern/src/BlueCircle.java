0462bab2b3039741f2fe02563bb0dd3ff2f88158
OverloadedWolf <overloadedwolf@gmail.com>
2017-10-12 04:45:42 -0400

    Commiting

---
+++ BridgePattern/src/BlueCircle.java
@@ -0,0 +1,6 @@
+public class BlueCircle implements DrawAPI{
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle [color: blue, radius: " + radius + ", x: " + x + ", y: " + y + "]");
+    }
+}
